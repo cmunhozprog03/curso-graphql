4e5fce9e826154b9721b02c580c4f4a5687455ce
cmunhozprog <cmunhozprog03@hotmail.com>
2020-09-30 21:31:44 -0300

    aula 10 completa

---
+++ schema-query/projeto/index.js
@@ -0,0 +1,26 @@
+const { ApolloServer, gql} = require('apollo-server')
+
+const typeDefs = gql`
+    # pontos de eentrada se dua API!
+    type Query {
+        ola: String!
+    }
+
+`
+const resolvers = {
+    Query: {
+        ola() {
+            return 'Bom dia!'
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+    
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+})
